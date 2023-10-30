a1d56bbf7c45a1a362e06f790e2e4ecda3ccbd9b
SanyaMar <125236859+SanyaMar@users.noreply.github.com>
2023-10-30 23:16:37 +0300

    Update pp.py

--- pp.py
+++ pp.py
@@ -26,5 +26,5 @@ def main() -> None:
     
 
 
-
+#ссылки на картинки
 #  python main.py https://kartinki.pics/pics/uploads/posts/2022-07/thumbs/1658296079_68-kartinkin-net-p-adskaya-gonchaya-grimm-art-oboi-73.jpg https://chakiris.club/uploads/posts/2022-11/1668605594_chakiris-club-p-adskaya-gonchaya-dnd-oboi-10.png https://kartinki.pics/pics/uploads/posts/2022-07/1658295978_8-kartinkin-net-p-adskaya-gonchaya-grimm-art-oboi-9.jpg https://kartinkin.net/pics/uploads/posts/2022-07/1658296325_62-kartinkin-net-p-adskaya-gonchaya-sverkhestestvennoe-art-ob-65.jpg https://krasivosti.pro/uploads/posts/2023-07/1688424066_krasivosti-pro-p-koshki-raznikh-tsvetov-oboi-54.jpg https://u.9111s.ru/uploads/202205/27/f2cd4d70a1ff2a2655a42dd69f56235b.jpg
